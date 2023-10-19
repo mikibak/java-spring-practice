1fc77279db1a8c5640d252fb2573db466deb125e
mikibak <107919074+mikibak@users.noreply.github.com>
2023-10-19 15:16:36 +0200

    Start Project 2 in Spring framework

---
+++ Project2SpringData/src/main/java/com/example/Project2SpringData/Project2SpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project2SpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2SpringDataApplication.class, args);
+	}
+
+}
---
+++ Project2SpringData/src/test/java/com/example/Project2SpringData/Project2SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project2SpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
