a1fa3721cf1a1a213106591e2f0de9e44b8de70d
mikibak <107919074+mikibak@users.noreply.github.com>
2023-10-24 12:14:38 +0200

    Start Project 1

---
+++ Project2SpringJPA/src/main/java/com/example/Project2SpringJPA/Project2SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project2SpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2SpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2SpringJpaApplication.class, args);
+	}
+
+}
---
+++ Project2SpringJPA/src/test/java/com/example/Project2SpringJPA/Project2SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project2SpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
